10d41781611d3f841252a74b3874e05e5e645d30
Ross Light <ross@zombiezen.com>
2010-05-27 14:49:34 -0700

    Added trig functions to fmath

--- src/fmath.go
+++ src/fmath.go
@@ -11,11 +11,27 @@ import "math"
 
 var Inf = float(math.Inf(1))
 
+type unaryFunc func(float64) float64
+
+func callUnary(f unaryFunc, n float) float {
+	return float(f(float64(n)))
+}
+
+type binaryFunc func(float64, float64) float64
+
+func callBinary(f binaryFunc, n1, n2 float) float {
+	return float(f(float64(n1), float64(n2)))
+}
+
 func Abs(f float) float {
 	if f >= 0 {
 		return f
 	}
 	return -f
+}
+
+func Cos(f float) float {
+	return callUnary(math.Cos, f)
 }
 
 func IsInf(n float) bool {
@@ -36,6 +52,18 @@ func Max(f1, f2 float) float {
 	return f2
 }
 
+func Mod(f1, f2 float) float {
+	return callBinary(math.Fmod, f1, f2)
+}
+
+func Sin(f float) float {
+	return callUnary(math.Sin, f)
+}
+
 func Sqrt(f float) float {
-	return float(math.Sqrt(float64(f)))
+	return callUnary(math.Sqrt, f)
+}
+
+func Tan(f float) float {
+	return callUnary(math.Tan, f)
 }
