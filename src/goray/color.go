ccab30961d9cb3ce0347aa47b9d358f0ed5f01b7
Ross Light <rlight2@gmail.com>
2010-05-27 09:48:46 -0700

    Added more interface magic to color package (Also had to export the RGB and RGBA fields to make it usable in other places)

--- src/goray/color.go
+++ src/goray/color.go
@@ -9,25 +9,61 @@ package color
 
 import "./fmath"
 
+type Alpha interface {
+	GetA() float
+}
+
 type Color interface {
 	GetR() float
 	GetG() float
 	GetB() float
-	GetA() float
+}
+
+type AlphaColor interface {
+	Color
+	Alpha
 }
 
 // RGB Color
 
 type RGB struct {
-	r, g, b float
+	R, G, B float
 }
 
-func NewRGB(r, g, b float) RGB { return RGB{r, g, b} }
-func DiscardAlpha(c Color) RGB { return NewRGB(c.GetR(), c.GetG(), c.GetB()) }
-func (c RGB) GetR() float      { return c.r }
-func (c RGB) GetG() float      { return c.g }
-func (c RGB) GetB() float      { return c.b }
-func (c RGB) GetA() float      { return 1.0 }
+func NewRGB(r, g, b float) RGB    { return RGB{r, g, b} }
+func DiscardAlpha(c Color) RGB    { return NewRGB(c.GetR(), c.GetG(), c.GetB()) }
+func (c *RGB) Init(r, g, b float) { c.R = r; c.G = g; c.B = b }
+func (c *RGB) Copy(src Color) {
+	c.R = src.GetR()
+	c.G = src.GetG()
+	c.B = src.GetB()
+}
+
+func (c RGB) GetR() float { return c.R }
+func (c RGB) GetG() float { return c.G }
+func (c RGB) GetB() float { return c.B }
+
+// RGBA colors
+
+type RGBA struct {
+	RGB
+	A float
+}
+
+func NewRGBA(r, g, b, a float) RGBA   { return RGBA{NewRGB(r, g, b), a} }
+func (c *RGBA) Init(r, g, b, a float) { c.RGB.Init(r, g, b); c.A = a }
+func (c *RGBA) Copy(src AlphaColor) {
+	c.RGB.Copy(src)
+	c.A = src.GetA()
+}
+
+func (c RGBA) GetA() float { return c.A }
+
+func (c RGBA) AlphaPremultiply() RGBA {
+	return NewRGBA(c.GetR()*c.A, c.GetG()*c.A, c.GetB()*c.A, c.A)
+}
+
+// Operations
 
 func IsBlack(c Color) bool {
 	return c.GetR() == 0 && c.GetG() == 0 && c.GetB() == 0
@@ -37,41 +73,61 @@ func GetEnergy(c Color) float {
 	return (c.GetR() + c.GetG() + c.GetB()) * 0.33333333333333
 }
 
-func InvertRGB(c Color) Color {
-	newColor := RGBA{a: c.GetA()}
+func Invert(c Color) Color {
+	newColor := NewRGB(0.0, 0.0, 0.0)
 	if c.GetR() != 0 {
-		newColor.RGB.r = 1.0 / c.GetR()
+		newColor.R = 1.0 / c.GetR()
 	}
 	if c.GetG() != 0 {
-		newColor.RGB.g = 1.0 / c.GetG()
+		newColor.G = 1.0 / c.GetG()
 	}
 	if c.GetB() != 0 {
-		newColor.RGB.b = 1.0 / c.GetB()
+		newColor.B = 1.0 / c.GetB()
 	}
 	return newColor
 }
 
-func AbsRGB(c Color) Color {
-	return NewRGBA(fmath.Abs(c.GetR()), fmath.Abs(c.GetG()), fmath.Abs(c.GetB()), c.GetA())
+func Abs(c Color) Color {
+	return NewRGB(fmath.Abs(c.GetR()), fmath.Abs(c.GetG()), fmath.Abs(c.GetB()))
 }
 
 func Add(c1, c2 Color) Color {
+	return NewRGB(c1.GetR()+c2.GetR(), c1.GetG()+c2.GetG(), c1.GetB()+c2.GetB())
+}
+
+func AddAlpha(c1, c2 AlphaColor) AlphaColor {
 	return NewRGBA(c1.GetR()+c2.GetR(), c1.GetG()+c2.GetG(), c1.GetB()+c2.GetB(), c1.GetA()+c2.GetA())
 }
 
 func Sub(c1, c2 Color) Color {
+	return NewRGB(c1.GetR()-c2.GetR(), c1.GetG()-c2.GetG(), c1.GetB()-c2.GetB())
+}
+
+func SubAlpha(c1, c2 AlphaColor) AlphaColor {
 	return NewRGBA(c1.GetR()-c2.GetR(), c1.GetG()-c2.GetG(), c1.GetB()-c2.GetB(), c1.GetA()-c2.GetA())
 }
 
 func Mul(c1, c2 Color) Color {
+	return NewRGB(c1.GetR()*c2.GetR(), c1.GetG()*c2.GetG(), c1.GetB()*c2.GetB())
+}
+
+func MulAlpha(c1, c2 AlphaColor) AlphaColor {
 	return NewRGBA(c1.GetR()*c2.GetR(), c1.GetG()*c2.GetG(), c1.GetB()*c2.GetB(), c1.GetA()*c2.GetA())
 }
 
 func ScalarMul(c Color, f float) Color {
+	return NewRGB(c.GetR()*f, c.GetG()*f, c.GetB()*f)
+}
+
+func ScalarMulAlpha(c AlphaColor, f float) AlphaColor {
 	return NewRGBA(c.GetR()*f, c.GetG()*f, c.GetB()*f, c.GetA()*f)
 }
 
 func ScalarDiv(c Color, f float) Color {
+	return NewRGB(c.GetR()/f, c.GetG()/f, c.GetB()/f)
+}
+
+func ScalarDivAlpha(c AlphaColor, f float) AlphaColor {
 	return NewRGBA(c.GetR()/f, c.GetG()/f, c.GetB()/f, c.GetA()/f)
 }
 
@@ -84,16 +140,11 @@ func Mix(a, b Color, point float) Color {
 	return Add(ScalarMul(a, point), ScalarMul(b, 1-point))
 }
 
-// RGBA colors
-
-type RGBA struct {
-	RGB
-	a float
-}
-
-func NewRGBA(r, g, b, a float) RGBA { return RGBA{NewRGB(r, g, b), a} }
-func (c RGBA) GetA() float          { return c.a }
-
-func (c RGBA) AlphaPremultiply() RGBA {
-	return NewRGBA(c.GetR()*c.a, c.GetG()*c.a, c.GetB()*c.a, c.a)
+func MixAlpha(a, b AlphaColor, point float) AlphaColor {
+	if point < 0 {
+		return b
+	} else if point > 1 {
+		return a
+	}
+	return AddAlpha(ScalarMulAlpha(a, point), ScalarMulAlpha(b, 1-point))
 }
