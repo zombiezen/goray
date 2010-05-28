261bb6e097e1155a4dcdb0e61ad24d5211a825c5
Ross Light <ross@zombiezen.com>
2010-05-27 18:26:32 -0700

    Added rough (but not compiling) main

--- src/main.go
+++ src/main.go
@@ -10,6 +10,7 @@ package main
 import (
     "flag"
     "fmt"
+    "./goray/scene"
 )
 
 func printInstructions() {
@@ -19,9 +20,11 @@ func printInstructions() {
 }
 
 func main() {
+    var err os.Error
+    
     help := flag.Bool("h", false, "display this help")
     format := flag.String("f", "png", "the output format")
-    outputPath := flag.String("o", "", "path for the output file")
+    outputPath := flag.String("o", "goray.png", "path for the output file")
     debug := flag.Int("d", 0, "set debug verbosity level")
     version := flag.Bool("v", false, "display the version")
     
@@ -42,5 +45,29 @@ func main() {
         return
     }
     
-    _, _, _ = format, outputPath, debug
+    _ = debug
+    f, err := os.Open(outputPath, os.O_WRONLY | os.O_CREAT, 0644)
+    defer f.Close()
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+        return
+    }
+    sc := scene.New()
+    
+    fmt.Println("Setting up scene...")
+    //ok := parseXMLFile(f, scene)
+    
+    fmt.Println("Rendering...")
+    outputImage := sc.Render()
+    
+    fmt.Println("Writing and finishing...")
+    switch *format {
+        case "png":
+            err = png.Encode(f, outputImage)
+    }
+    
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Error while writing: %v\n", err)
+        return
+    }
 }
