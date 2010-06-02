07baf22cf0ffaff99fadd4bb26c3f43c599046d7
Ross Light <ross@zombiezen.com>
2010-06-02 13:15:13 -0700

    Documented bound.go

--- src/goray/bound.go
+++ src/goray/bound.go
@@ -5,6 +5,10 @@
 //  Created by Ross Light on 2010-05-23.
 //
 
+/*
+   The goray/bound package provides a bounding box type, along with various
+   manipulation operaions.
+*/
 package bound
 
 import (
@@ -12,14 +16,20 @@ import (
 	"./goray/vector"
 )
 
+/*
+   Bound is a simple bounding box.
+   It should only be passed around as a pointer.
+*/
 type Bound struct {
 	a, g vector.Vector3D
 }
 
-func New(a, g vector.Vector3D) *Bound {
-	return &Bound{a, g}
+/* New creates a new bounding box from the two points given. */
+func New(min, max vector.Vector3D) *Bound {
+	return &Bound{min, max}
 }
 
+/* Union creates a new bounding box that contains the two bounds. */
 func Union(b1, b2 *Bound) *Bound {
 	newBound := &Bound{}
 	newBound.a.X = fmath.Min(b1.a.X, b2.a.X)
@@ -31,23 +41,26 @@ func Union(b1, b2 *Bound) *Bound {
 	return newBound
 }
 
+/* Get returns the minimum and maximum points that define the box. */
 func (b *Bound) Get() (a, g vector.Vector3D) { return b.a, b.g }
-func (b *Bound) GetMin() vector.Vector3D     { return b.a }
-func (b *Bound) GetMax() vector.Vector3D     { return b.g }
-func (b *Bound) Set(a, g vector.Vector3D)   { b.a = a; b.g = g }
+/* Set changes the minimum and maximum points that define the box. */
+func (b *Bound) Set(a, g vector.Vector3D) { b.a = a; b.g = g }
 
-func (b *Bound) SetMinX(x float) { b.a.X = x }
-func (b *Bound) SetMinY(y float) { b.a.Y = y }
-func (b *Bound) SetMinZ(z float) { b.a.Z = z }
-func (b *Bound) SetMaxX(x float) { b.g.X = x }
-func (b *Bound) SetMaxY(y float) { b.g.Y = y }
-func (b *Bound) SetMaxZ(z float) { b.g.Z = z }
+func (b *Bound) GetMin() vector.Vector3D { return b.a }
+func (b *Bound) GetMax() vector.Vector3D { return b.g }
+func (b *Bound) SetMinX(x float)         { b.a.X = x }
+func (b *Bound) SetMinY(y float)         { b.a.Y = y }
+func (b *Bound) SetMinZ(z float)         { b.a.Z = z }
+func (b *Bound) SetMaxX(x float)         { b.g.X = x }
+func (b *Bound) SetMaxY(y float)         { b.g.Y = y }
+func (b *Bound) SetMaxZ(z float)         { b.g.Z = z }
 
-// Cross checks whether a given ray crosses the bound.
-// from specifies a point where the ray starts.
-// ray specifies the direction the ray is in.
-// dist is the maximum distance that this method will check.  Pass in fmath.Inf
-// to remove the check.
+/*
+   Cross checks whether a given ray crosses the bound.
+   from specifies a point where the ray starts.
+   ray specifies the direction the ray is in.
+   dist is the maximum distance that this method will check.  Pass in fmath.Inf to remove the check.
+*/
 func (b *Bound) Cross(from, ray vector.Vector3D, dist float) (crosses bool, enter, leave float) {
 	a0, a1 := b.a, b.g
 	p := vector.Sub(from, a0)
@@ -103,6 +116,7 @@ func (b *Bound) Cross(from, ray vector.Vector3D, dist float) (crosses bool, ente
 	return
 }
 
+/* GetVolume calculates the volume of the bounding box */
 func (b *Bound) GetVolume() float {
 	return (b.g.Y - b.a.Y) * (b.g.X - b.a.X) * (b.g.Z - b.a.Z)
 }
@@ -111,6 +125,7 @@ func (b *Bound) GetXLength() float { return b.g.X - b.a.X }
 func (b *Bound) GetYLength() float { return b.g.Y - b.a.Y }
 func (b *Bound) GetZLength() float { return b.g.Z - b.a.Z }
 
+/* Include modifies the bounding box so that it contains the specified point */
 func (b *Bound) Include(p vector.Vector3D) {
 	b.a.X = fmath.Min(b.a.X, p.X)
 	b.a.Y = fmath.Min(b.a.Y, p.Y)
@@ -120,6 +135,7 @@ func (b *Bound) Include(p vector.Vector3D) {
 	b.g.Z = fmath.Max(b.g.Z, p.Z)
 }
 
+/* Includes returns whether a given point is in the bounding box */
 func (b *Bound) Includes(p vector.Vector3D) bool {
 	return (p.X >= b.a.X && p.X <= b.g.X &&
 		p.Y >= b.a.Y && p.Y <= b.g.Y &&
@@ -134,6 +150,7 @@ func (b *Bound) GetCenterX() float { return (b.g.X + b.a.X) * 0.5 }
 func (b *Bound) GetCenterY() float { return (b.g.Y + b.a.Y) * 0.5 }
 func (b *Bound) GetCenterZ() float { return (b.g.Z + b.a.Z) * 0.5 }
 
+/* Grow increases the size of the bounding box by d on all sides.  The center will remain the same. */
 func (b *Bound) Grow(d float) {
 	b.a.X -= d
 	b.a.Y -= d
