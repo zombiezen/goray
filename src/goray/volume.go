3c88ae45688752d5b3f098bf23034f9a8517f29e
Ross Light <ross@zombiezen.com>
2010-06-03 11:33:53 -0700

    Ran gofmt on volume.go

--- src/goray/volume.go
+++ src/goray/volume.go
@@ -18,24 +18,24 @@ import (
 
 /* Region defines a volumetric effect */
 type Region interface {
-    /* SigmaA returns the amount of light absorption in the volume. */
+	/* SigmaA returns the amount of light absorption in the volume. */
 	SigmaA(p, v vector.Vector3D) color.Color
-    /* SigmaS returns the amount of light scattering in the volume. */
+	/* SigmaS returns the amount of light scattering in the volume. */
 	SigmaS(p, v vector.Vector3D) color.Color
-    /* Emissions returns the amout of light the volume emits. */
+	/* Emissions returns the amout of light the volume emits. */
 	Emission(p, v vector.Vector3D) color.Color
 	SigmaT(p, v vector.Vector3D) color.Color
 
-    /* Attenuation returns how much the volumetric effect dissipates over distance. */
+	/* Attenuation returns how much the volumetric effect dissipates over distance. */
 	Attenuation(p vector.Vector3D, l light.Light) float
 
 	P(l, s vector.Vector3D) float
 
 	Tau(r ray.Ray, step, offset float) color.Color
 
-    /* Intersect returns whether a ray intersects the volume. */
+	/* Intersect returns whether a ray intersects the volume. */
 	Intersect(r ray.Ray) (t0, t1 float, ok bool)
 
-    /* GetBound returns the bounding box of the volume. */
+	/* GetBound returns the bounding box of the volume. */
 	GetBound() *bound.Bound
 }
