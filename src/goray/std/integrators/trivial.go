c05e595a0f61a3826d683250dd6d053849e610b5
Ross Light <ross@zombiezen.com>
2010-06-03 20:58:27 -0700

    Completed the trivial integrator

--- src/goray/std/integrators/trivial.go
+++ src/goray/std/integrators/trivial.go
@@ -8,41 +8,67 @@
 package trivial
 
 import (
-    "os"
-    "./goray/color"
-    "./goray/integrator"
-    "./goray/ray"
-    "./goray/render"
-    "./goray/scene"
+	"os"
+	"./goray/color"
+	"./goray/integrator"
+	"./goray/ray"
+	"./goray/render"
+	"./goray/scene"
+	"./goray/vector"
 )
 
 type trivial struct {
-    sc *scene.Scene
+	sc *scene.Scene
 }
 
-func New() integrator.SurfaceIntegrator { return &trivial{} }
-func (ti *trivial) SurfaceIntegrator() {}
+func New() integrator.SurfaceIntegrator    { return &trivial{} }
+func (ti *trivial) SurfaceIntegrator()     {}
 func (ti *trivial) SetScene(s interface{}) { ti.sc = s.(*scene.Scene) }
-func (ti *trivial) Preprocess() os.Error { return nil }
+func (ti *trivial) Preprocess() os.Error   { return nil }
 
 func (ti *trivial) Integrate(s *render.State, r ray.Ray) color.AlphaColor {
-    if _, hit, _ := ti.sc.Intersect(r); hit {
-        return color.NewRGBA(1.0, 1.0, 1.0, 1.0)
-    }
-    return color.NewRGBA(0.1, 0.1, 0.1, 0.0)
+	if _, hit, _ := ti.sc.Intersect(r); hit {
+		return color.NewRGBA(1.0, 1.0, 1.0, 1.0)
+	}
+	return color.NewRGBA(0.1, 0.1, 0.1, 0.0)
 }
 
 func (ti *trivial) Render() <-chan render.Fragment {
-    cam := ti.sc.GetCamera()
-    ch := make(chan render.Fragment)
-    for x := 0; x < cam.ResolutionX(); x++ {
-        for y := 0; y < cam.ResolutionY(); y++ {
-            go func() {
-                r, _ := cam.ShootRay(float(x), float(y), 0, 0)
-                color := ti.Integrate(nil, r)
-                ch <- render.Fragment{X: x, Y: y, Color: color}
-            }()
-        }
-    }
-    return ch
+	cam := ti.sc.GetCamera()
+	w, h := cam.ResolutionX(), cam.ResolutionY()
+	ch := make(chan render.Fragment, 1000)
+	control := make(chan bool)
+	go func() {
+		for x := 0; x < w; x++ {
+			for y := 0; y < h; y++ {
+				go func(x, y int) {
+					// Set up state
+					state := &render.State{}
+					state.Init(nil)
+					state.PixelNumber = y*w + x
+					state.ScreenPos = vector.New(2.0*float(x)/float(w)-1.0, -2.0*float(y)/float(h)+1.0, 0.0)
+					state.Time = 0.0
+					// Shoot ray
+					r, _ := cam.ShootRay(float(x), float(y), 0, 0)
+					r.TMin, r.TMax = 0.0, -1.0 // TODO: Add this to Ray
+					// Integrate
+					color := ti.Integrate(state, r)
+					ch <- render.Fragment{X: x, Y: y, Color: color}
+					control <- true
+				}(x, y)
+			}
+		}
+	}()
+	go func() {
+		pixrendered := 0
+		pixcount := w * h
+		for _ = range control {
+			pixrendered++
+			if pixrendered >= pixcount {
+				close(control)
+				close(ch)
+			}
+		}
+	}()
+	return ch
 }
--- src/main.go
+++ src/main.go
@@ -8,80 +8,82 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "os"
-    "image/png"
-    "./goray/camera"
-    "./goray/object"
-    "./goray/primitive"
-    "./goray/scene"
-    "./goray/vector"
+	"flag"
+	"fmt"
+	"os"
+	"image/png"
+	"./goray/camera"
+	"./goray/object"
+	"./goray/primitive"
+	"./goray/scene"
+	"./goray/vector"
+	trivialInt "./goray/std/integrators/trivial"
 )
 
 func printInstructions() {
-    fmt.Println("USAGE: goray [OPTION]... FILE")
-    fmt.Println("OPTIONS:")
-    flag.PrintDefaults()
+	fmt.Println("USAGE: goray [OPTION]... FILE")
+	fmt.Println("OPTIONS:")
+	flag.PrintDefaults()
 }
 
 func main() {
-    var err os.Error
-    
-    help := flag.Bool("h", false, "display this help")
-    format := flag.String("f", "png", "the output format")
-    outputPath := flag.String("o", "goray.png", "path for the output file")
-    debug := flag.Int("d", 0, "set debug verbosity level")
-    version := flag.Bool("v", false, "display the version")
-    
-    flag.Usage = printInstructions
-    flag.Parse()
-    
-    switch {
-        case *help:
-            printInstructions()
-            return
-        case *version:
-            fmt.Println("This is SPARTA!")
-            return
-    }
-    
-    if flag.NArg() != 1 {
-        printInstructions()
-        return
-    }
-    
-    _ = debug
-    f, err := os.Open(*outputPath, os.O_WRONLY | os.O_CREAT, 0644)
-    defer f.Close()
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-        return
-    }
-    sc := scene.New()
-    
-    fmt.Println("Setting up scene...")
-    // We should be doing this:
-    //ok := parseXMLFile(f, scene)
-    // For now, we'll do this:
-    sc.SetCamera(camera.NewOrtho(vector.New(0.0, 0.0, 10.0), vector.New(0.0, 0.0, 0.0), vector.New(0.0, 1.0, 0.0), 640, 480, 1.0, 1.0))
-    sc.AddObject(object.NewPrimitive(primitive.NewSphere(vector.New(0.0, 0.0, 0.0), 1.0, nil)))
-    
-    fmt.Println("Rendering...")
-    outputImage, err := sc.Render()
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Rendering error: %v\n", err)
-        return
-    }
-    
-    fmt.Println("Writing and finishing...")
-    switch *format {
-        case "png":
-            err = png.Encode(f, outputImage)
-    }
-    
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Error while writing: %v\n", err)
-        return
-    }
+	var err os.Error
+
+	help := flag.Bool("h", false, "display this help")
+	format := flag.String("f", "png", "the output format")
+	outputPath := flag.String("o", "goray.png", "path for the output file")
+	debug := flag.Int("d", 0, "set debug verbosity level")
+	version := flag.Bool("v", false, "display the version")
+
+	flag.Usage = printInstructions
+	flag.Parse()
+
+	switch {
+	case *help:
+		printInstructions()
+		return
+	case *version:
+		fmt.Println("This is SPARTA!")
+		return
+	}
+
+	if flag.NArg() != 1 {
+		printInstructions()
+		return
+	}
+
+	_ = debug
+	f, err := os.Open(*outputPath, os.O_WRONLY|os.O_CREAT, 0644)
+	defer f.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		return
+	}
+	sc := scene.New()
+
+	fmt.Println("Setting up scene...")
+	// We should be doing this:
+	//ok := parseXMLFile(f, scene)
+	// For now, we'll do this:
+	sc.SetCamera(camera.NewOrtho(vector.New(0.0, 0.0, 10.0), vector.New(0.0, 0.0, 0.0), vector.New(0.0, 1.0, 10.0), 100, 100, 1.0, 2.0))
+	sc.AddObject(object.NewPrimitive(primitive.NewSphere(vector.New(0.0, 0.0, 0.0), 1.0, nil)))
+	sc.SetSurfaceIntegrator(trivialInt.New())
+
+	fmt.Println("Rendering...")
+	outputImage, err := sc.Render()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Rendering error: %v\n", err)
+		return
+	}
+
+	fmt.Println("Writing and finishing...")
+	switch *format {
+	case "png":
+		err = png.Encode(f, outputImage)
+	}
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error while writing: %v\n", err)
+		return
+	}
 }
