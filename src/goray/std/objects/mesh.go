c41d82e8d2e84ddc313f90a7ed782b85265b36dd
Ross Light <ross@zombiezen.com>
2010-06-05 11:59:24 -0700

    Added Triangle.IntersectsBound implementation

--- src/goray/std/objects/mesh.go
+++ src/goray/std/objects/mesh.go
@@ -91,8 +91,8 @@ func NewTriangle(a, b, c int, m *Mesh) *Triangle {
 	return &Triangle{
 		va: a, vb: b, vc: c,
 		na: -1, nb: -1, nc: -1,
-//		index: -1,
-//		mesh:  m,
+		//		index: -1,
+		//		mesh:  m,
 	}
 }
 
@@ -105,7 +105,7 @@ func (tri *Triangle) getNormals() (a, b, c vector.Vector3D) {
 		if i >= 0 && tri.mesh.normals != nil {
 			return tri.mesh.normals[i]
 		}
-        return tri.normal
+		return tri.normal
 	}
 	return f(tri.na), f(tri.nb), f(tri.nc)
 }
@@ -197,7 +197,19 @@ func (tri *Triangle) GetBound() *bound.Bound {
 	return bound.New(minPt, maxPt)
 }
 
-func (tri *Triangle) IntersectsBound(*bound.Bound) bool                                       { return true }
+func (tri *Triangle) IntersectsBound(bd *bound.Bound) bool {
+	var points [3][3]float
+	a, b, c := tri.getVertices()
+
+	for i := 0; i < 3; i++ {
+		points[0][i] = a.GetComponent(i)
+		points[1][i] = b.GetComponent(i)
+		points[2][i] = c.GetComponent(i)
+	}
+	ctr := bd.GetCenter()
+	return triBoxOverlap([3]float{ctr.X, ctr.Y, ctr.Z}, bd.GetHalfSize(), points)
+}
+
 func (tri *Triangle) HasClippingSupport() bool                                                { return false }
 func (tri *Triangle) ClipToBound(bound [2][3]float, axis int) (clipped *bound.Bound, ok bool) { return }
 
@@ -219,4 +231,190 @@ func (tri *Triangle) GetSurfaceArea() float {
 	a, b, c := tri.getVertices()
 	edge1, edge2 := vector.Sub(b, a), vector.Sub(c, a)
 	return vector.Cross(edge1, edge2).Length() * 0.5
+}
+
+// Triangle bound intersection methods
+// Note that a lot of functionality gets inlined for efficiency reasons.
+
+func planeBoxOverlap(normal, vert, maxbox [3]float) bool {
+	var vmin, vmax [3]float
+
+	for q := 0; q < 3; q++ {
+		v := vert[q]
+		if normal[q] > 0.0 {
+			vmin[q] = -maxbox[q] - v
+			vmax[q] = maxbox[q] - v
+		} else {
+			vmin[q] = maxbox[q] - v
+			vmax[q] = -maxbox[q] - v
+		}
+	}
+
+	if normal[0]*vmin[0]+normal[1]*vmin[1]+normal[2]*vmin[2] > 0 {
+		return false
+	}
+	if normal[0]*vmax[0]+normal[1]*vmax[1]+normal[2]*vmax[2] >= 0 {
+		return true
+	}
+
+	return false
+}
+
+func triBoxOverlap(boxcenter, boxhalfsize [3]float, verts [3][3]float) bool {
+	const X = 0
+	const Y = 1
+	const Z = 2
+	var normal, f [3]float
+	var v, e [3][3]float
+	var min, max float
+
+	axisTest := func(axis, i, j, edgeNum int) bool {
+		var axis1, axis2 int
+		var a, b, fa, fb float
+		var p1, p2, rad, min, max float
+		var v1, v2 []float
+
+		switch axis {
+		case X:
+			axis1, axis2 = Y, Z
+		case Y:
+			axis1, axis2 = X, Z
+		case Z:
+			axis1, axis2 = X, Y
+		}
+
+		v1, v2 = &v[i], &v[j]
+		a, b = e[edgeNum][axis2], e[edgeNum][axis1]
+		fa, fb = f[axis2], f[axis1]
+
+		p1 = -a*v1[axis1] + b*v1[axis2]
+		p2 = -a*v2[axis1] + b*v2[axis2]
+		if p1 < p2 {
+			min, max = p1, p2
+		} else {
+			min, max = p2, p1
+		}
+		rad = fa*boxhalfsize[axis1] + fb*boxhalfsize[axis2]
+		return min <= rad && max >= -rad
+	}
+
+	// Move everything so that the boxcenter is in (0, 0, 0)
+	v[0][X] = verts[0][X] - boxcenter[X]
+	v[0][Y] = verts[0][Y] - boxcenter[Y]
+	v[0][Z] = verts[0][Z] - boxcenter[Z]
+
+	v[1][X] = verts[1][X] - boxcenter[X]
+	v[1][Y] = verts[1][Y] - boxcenter[Y]
+	v[1][Z] = verts[1][Z] - boxcenter[Z]
+
+	v[2][X] = verts[2][X] - boxcenter[X]
+	v[2][Y] = verts[2][Y] - boxcenter[Y]
+	v[2][Z] = verts[2][Z] - boxcenter[Z]
+
+	// Compute triangle edges
+	e[0][X] = v[1][X] - v[0][X]
+	e[0][Y] = v[1][Y] - v[0][Y]
+	e[0][Z] = v[1][Z] - v[0][Z]
+
+	e[1][X] = v[2][X] - v[1][X]
+	e[1][Y] = v[2][Y] - v[1][Y]
+	e[1][Z] = v[2][Z] - v[1][Z]
+
+	e[2][X] = v[0][X] - v[2][X]
+	e[2][Y] = v[0][Y] - v[2][Y]
+	e[2][Z] = v[0][Z] - v[2][Z]
+
+	// Run the nine tests
+	f = [3]float{fmath.Abs(e[0][X]), fmath.Abs(e[0][Y]), fmath.Abs(e[0][Z])}
+	if !axisTest(X, 0, 1, 0) {
+		return false
+	}
+	if !axisTest(Y, 0, 2, 0) {
+		return false
+	}
+	if !axisTest(Z, 1, 2, 0) {
+		return false
+	}
+
+	f = [3]float{fmath.Abs(e[1][X]), fmath.Abs(e[1][Y]), fmath.Abs(e[1][Z])}
+	if !axisTest(X, 0, 1, 1) {
+		return false
+	}
+	if !axisTest(Y, 0, 2, 1) {
+		return false
+	}
+	if !axisTest(Z, 0, 1, 1) {
+		return false
+	}
+
+	f = [3]float{fmath.Abs(e[2][X]), fmath.Abs(e[2][Y]), fmath.Abs(e[2][Z])}
+	if !axisTest(X, 0, 2, 2) {
+		return false
+	}
+	if !axisTest(Y, 0, 1, 2) {
+		return false
+	}
+	if !axisTest(Z, 1, 2, 2) {
+		return false
+	}
+
+	// First test overlap in the x,y,z directions
+	// This is equivalent to testing a minimal AABB
+	min, max = v[0][X], v[0][X]
+	if v[1][X] < min {
+		min = v[1][X]
+	}
+	if v[1][X] > max {
+		max = v[1][X]
+	}
+	if v[2][X] < min {
+		min = v[2][X]
+	}
+	if v[2][X] > max {
+		max = v[2][X]
+	}
+	if min > boxhalfsize[X] || max < -boxhalfsize[X] {
+		return false
+	}
+
+	min, max = v[0][Y], v[0][Y]
+	if v[1][Y] < min {
+		min = v[1][Y]
+	}
+	if v[1][Y] > max {
+		max = v[1][Y]
+	}
+	if v[2][Y] < min {
+		min = v[2][Y]
+	}
+	if v[2][Y] > max {
+		max = v[2][Y]
+	}
+	if min > boxhalfsize[Y] || max < -boxhalfsize[Y] {
+		return false
+	}
+
+	min, max = v[0][Z], v[0][Z]
+	if v[1][Z] < min {
+		min = v[1][Z]
+	}
+	if v[1][Z] > max {
+		max = v[1][Z]
+	}
+	if v[2][Z] < min {
+		min = v[2][Z]
+	}
+	if v[2][Z] > max {
+		max = v[2][Z]
+	}
+	if min > boxhalfsize[Z] || max < -boxhalfsize[Z] {
+		return false
+	}
+
+	// Test if the box intersects the plane of the triangle
+	// Plane equation of triangle: normal * x + d = 0
+	normal[0] = e[0][1]*e[1][2] - e[0][2]*e[1][1]
+	normal[1] = e[0][2]*e[1][0] - e[0][0]*e[1][2]
+	normal[2] = e[0][0]*e[1][1] - e[0][1]*e[1][0]
+	return planeBoxOverlap(normal, v[0], boxhalfsize)
 }
