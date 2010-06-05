9ea09d85f2c3ba07c192c6185d2632f1b986cc49
Ross Light <ross@zombiezen.com>
2010-06-05 14:13:37 -0700

    Simplified Object3D interface (and separated sampling)

--- src/goray/object.go
+++ src/goray/object.go
@@ -5,61 +5,43 @@
 //  Created by Ross Light on 2010-05-23.
 //
 
-/* The goray/object package provides an object type--a collection of primitives. */
+/* The goray/object package provides an object type—a collection of primitives. */
 package object
 
 import (
-	"./goray/light"
 	"./goray/primitive"
-	"./goray/surface"
 	"./goray/vector"
 )
 
-/* Object3D is a collection of primitives */
+/* Object3D is a collection of primitives. */
 type Object3D interface {
 	/* GetPrimitives retrieves all of the primitives for this object. */
 	GetPrimitives() []primitive.Primitive
-	/* EvalVmap evaluates a vertex map (or equivalent for other geometry types if available) */
-	EvalVmap(sp surface.Point, id uint, val []float) (dim int)
-	/* SetLight associates a light source with this object */
-	SetLight(light.Light)
-	/* CanSample indicates whether the object's surface can be sampled */
-	CanSample() bool
-	/* EnableSampling tries to enable sampling (may require additional memory and preprocessing time, if supported) */
-	EnableSampling() bool
-	/* Sample takes a sample of the object's surface */
-	Sample(s1, s2 float) (p, n vector.Vector3D)
 	/* IsVisible indicates whether the object is shown in the scene. */
 	IsVisible() bool
-	/* SetVisible changes whether the object is shown in the scene. */
-	SetVisible(bool)
 }
 
-type primitiveObject struct {
-	prim   primitive.Primitive
-	light  light.Light
-	hidden bool
+/* Samplable defines an interface for sampling a surface. */
+type Samplable interface {
+	/* EnableSampling tries to enable sampling (may require additional memory and preprocessing time). */
+	EnableSampling() bool
+	/* Sample takes a sample of the object's surface. */
+	Sample(s1, s2 float) (p, n vector.Vector3D)
 }
 
-/* NewPrimitive creates an object that contains only one primitive. */
-func NewPrimitive(p primitive.Primitive) Object3D {
-	return &primitiveObject{p, nil, false}
+/* SamplableObject3D is the set of three-dimensional objects that can sample their surfaces. */
+type SamplableObject3D interface {
+	Object3D
+	Samplable
 }
 
-func (o *primitiveObject) GetPrimitives() []primitive.Primitive {
-	return []primitive.Primitive{o.prim}
+/* PrimitiveObject is a wrapper type that allows a single primitive to act as an object. */
+type PrimitiveObject struct {
+    Primitive primitive.Primitive
 }
 
-func (o *primitiveObject) SetLight(l light.Light) { o.light = l }
-func (o *primitiveObject) CanSample() bool        { return false }
-func (o *primitiveObject) EnableSampling() bool   { return false }
-func (o *primitiveObject) IsVisible() bool        { return !o.hidden }
-func (o *primitiveObject) SetVisible(v bool)      { o.hidden = !v }
-
-func (o *primitiveObject) EvalVmap(sp surface.Point, id uint, val []float) int {
-	return 0
+func (o PrimitiveObject) GetPrimitives() []primitive.Primitive {
+	return []primitive.Primitive{o.Primitive}
 }
 
-func (o *primitiveObject) Sample(s1, s2 float) (p, n vector.Vector3D) {
-	return
-}
+func (o PrimitiveObject) IsVisible() bool { return true }
--- src/main.go
+++ src/main.go
@@ -12,18 +12,18 @@ import (
 	"fmt"
 	"os"
 	"image/png"
-    "runtime"
-    "time"
+	"runtime"
+	"time"
 )
 
 import (
-    "./buildversion"
+	"./buildversion"
 	"./goray/camera"
 	"./goray/object"
 	"./goray/primitive"
 	"./goray/scene"
 	"./goray/vector"
-    "./goray/version"
+	"./goray/version"
 	trivialInt "./goray/std/integrators/trivial"
 )
 
@@ -34,30 +34,30 @@ func printInstructions() {
 }
 
 func printVersion() {
-    fmt.Printf("goray v%s - The Concurrent Raytracer\n", version.GetString())
-    // Copyright notice
-    fmt.Println("Copyright © 2005 Mathias Wein, Alejandro Conty, and Alfredo de Greef")
-    fmt.Println("Copyright © 2010 Ross Light")
-    fmt.Println()
-    fmt.Println("Based on the excellent YafaRay Ray-Tracer by Mathias Wein, Alejandro Conty, and")
-    fmt.Println("Alfredo de Greef.")
-    fmt.Println("Go rewrite by Ross Light")
-    fmt.Println()
-    fmt.Println("goray comes with ABSOLUTELY NO WARRANTY.  goray is free software, and you are")
-    fmt.Println("welcome to redistribute it under the conditions of the GNU Lesser General")
-    fmt.Println("Public License v3, or (at your option) any later version.")
-    fmt.Println()
-    // Build information
-    if buildversion.Source == "bzr" {
-        fmt.Printf("Built from \"%s\" branch\n", buildversion.BranchNickname)
-        fmt.Printf("  Revision: %s [%s]\n", buildversion.RevNo, buildversion.RevID)
-        if buildversion.CleanWC == 0 {
-            fmt.Printf("  With local modifications\n")
-        }
-    } else {
-        fmt.Println("Built from a source archive")
-    }
-    fmt.Printf("Go runtime: %s\n", runtime.Version())
+	fmt.Printf("goray v%s - The Concurrent Raytracer\n", version.GetString())
+	// Copyright notice
+	fmt.Println("Copyright © 2005 Mathias Wein, Alejandro Conty, and Alfredo de Greef")
+	fmt.Println("Copyright © 2010 Ross Light")
+	fmt.Println()
+	fmt.Println("Based on the excellent YafaRay Ray-Tracer by Mathias Wein, Alejandro Conty, and")
+	fmt.Println("Alfredo de Greef.")
+	fmt.Println("Go rewrite by Ross Light")
+	fmt.Println()
+	fmt.Println("goray comes with ABSOLUTELY NO WARRANTY.  goray is free software, and you are")
+	fmt.Println("welcome to redistribute it under the conditions of the GNU Lesser General")
+	fmt.Println("Public License v3, or (at your option) any later version.")
+	fmt.Println()
+	// Build information
+	if buildversion.Source == "bzr" {
+		fmt.Printf("Built from \"%s\" branch\n", buildversion.BranchNickname)
+		fmt.Printf("  Revision: %s [%s]\n", buildversion.RevNo, buildversion.RevID)
+		if buildversion.CleanWC == 0 {
+			fmt.Printf("  With local modifications\n")
+		}
+	} else {
+		fmt.Println("Built from a source archive")
+	}
+	fmt.Printf("Go runtime: %s\n", runtime.Version())
 }
 
 func main() {
@@ -66,8 +66,8 @@ func main() {
 	showHelp := flag.Bool("help", false, "display this help")
 	format := flag.String("f", "png", "the output format")
 	outputPath := flag.String("o", "goray.png", "path for the output file")
-    width := flag.Int("w", 100, "the output width")
-    height := flag.Int("h", 100, "the output height")
+	width := flag.Int("w", 100, "the output width")
+	height := flag.Int("h", 100, "the output height")
 	debug := flag.Int("d", 0, "set debug verbosity level")
 	showVersion := flag.Bool("v", false, "display the version")
 
@@ -79,15 +79,15 @@ func main() {
 		printInstructions()
 		return
 	case *showVersion:
-        printVersion()
+		printVersion()
 		return
 	}
 
-    // Eventually, we will take an input file.
-//	if flag.NArg() != 1 {
-//		printInstructions()
-//		return
-//	}
+	// Eventually, we will take an input file.
+	//	if flag.NArg() != 1 {
+	//		printInstructions()
+	//		return
+	//	}
 
 	_ = debug
 	f, err := os.Open(*outputPath, os.O_WRONLY|os.O_CREAT, 0644)
@@ -103,18 +103,18 @@ func main() {
 	//ok := parseXMLFile(f, scene)
 	// For now, we'll do this:
 	sc.SetCamera(camera.NewOrtho(vector.New(0.0, 0.0, 10.0), vector.New(0.0, 0.0, 0.0), vector.New(0.0, 1.0, 10.0), *width, *height, 1.0, 2.0))
-	sc.AddObject(object.NewPrimitive(primitive.NewSphere(vector.New(0.0, 0.0, 0.0), 1.0, nil)))
+	sc.AddObject(object.PrimitiveObject{primitive.NewSphere(vector.New(0.0, 0.0, 0.0), 1.0, nil)})
 	sc.SetSurfaceIntegrator(trivialInt.New())
 
 	fmt.Println("Rendering...")
-    startTime := time.Nanoseconds()
+	startTime := time.Nanoseconds()
 	outputImage, err := sc.Render()
-    endTime := time.Nanoseconds()
+	endTime := time.Nanoseconds()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Rendering error: %v\n", err)
 		return
 	}
-    fmt.Printf("Render finished in %.3fs\n", float(endTime - startTime) * 1e-9)
+	fmt.Printf("Render finished in %.3fs\n", float(endTime-startTime)*1e-9)
 
 	fmt.Println("Writing and finishing...")
 	switch *format {
