ed6bb58243ef0534be7be0e6c1a664cfacaf3759
Ross Light <ross@zombiezen.com>
2010-06-05 09:22:11 -0700

    Filled in (most of) Triangle.GetSurface

--- src/goray/std/objects/mesh.go
+++ src/goray/std/objects/mesh.go
@@ -37,7 +37,7 @@ func New(ntris int) (mesh *Mesh) {
 	mesh = new(Mesh)
 	mesh.triangles = make([]*Triangle, 0, ntris)
 	mesh.vertices = make([]vector.Vector3D, 0, ntris*3)
-	mesh.normals = make([]vector.Vector3D, 0)
+	mesh.normals = nil
 	return
 }
 
@@ -71,14 +71,16 @@ func (mesh *Mesh) AddTriangle(t *Triangle) {
 		copy(newTris, mesh.triangles)
 		mesh.triangles = newTris
 	}
+	t.index = len(mesh.triangles)
 	mesh.triangles = mesh.triangles[0 : len(mesh.triangles)+1]
-	mesh.triangles[len(mesh.triangles)-1] = t
+	mesh.triangles[t.index] = t
 }
 
 /* Triangle stores information for a single triangle. */
 type Triangle struct {
 	va, vb, vc int // va, vb, and vc are the vertex indices in the mesh's array.
 	na, nb, nc int // na, nb, and nc are the normal indices in the mesh's array (if the face is smooth)
+	index      int
 	normal     vector.Vector3D
 	material   material.Material
 	mesh       *Mesh
@@ -89,7 +91,8 @@ func NewTriangle(a, b, c int, m *Mesh) *Triangle {
 	return &Triangle{
 		va: a, vb: b, vc: c,
 		na: -1, nb: -1, nc: -1,
-		mesh: m,
+//		index: -1,
+//		mesh:  m,
 	}
 }
 
@@ -97,11 +100,21 @@ func (tri *Triangle) getVertices() (a, b, c vector.Vector3D) {
 	return tri.mesh.vertices[tri.va], tri.mesh.vertices[tri.vb], tri.mesh.vertices[tri.vc]
 }
 
+func (tri *Triangle) getNormals() (a, b, c vector.Vector3D) {
+	f := func(i int) vector.Vector3D {
+		if i >= 0 && tri.mesh.normals != nil {
+			return tri.mesh.normals[i]
+		}
+        return tri.normal
+	}
+	return f(tri.na), f(tri.nb), f(tri.nc)
+}
+
 func (tri *Triangle) Intersect(r ray.Ray) (coll primitive.Collision) {
 	// Tomas Möller and Ben Trumbore ray intersection scheme
 	// Ross adds: This is based on an ACM white paper which I don't have access to.
 	// I'm just going to smile, nod, and copy the code.  Code monkey very diligent.
-    coll.Ray = r
+	coll.Ray = r
 	a, b, c := tri.getVertices()
 
 	edge1, edge2 := vector.Sub(b, a), vector.Sub(c, a)
@@ -122,17 +135,58 @@ func (tri *Triangle) Intersect(r ray.Ray) (coll primitive.Collision) {
 		return
 	}
 
-    coll.Primitive = tri
+	coll.Primitive = tri
 	coll.RayDepth = vector.Dot(edge2, qvec) * invDet
-    coll.UserData = interface{}([2]float{u, v})
+	coll.UserData = interface{}([2]float{u, v})
 	return
 }
 
 func (tri *Triangle) GetSurface(coll primitive.Collision) (sp surface.Point) {
 	sp.GeometricNormal = tri.normal
-    dat := coll.UserData.([2]float)
-    _ = dat
-	// TODO: This is gonna be ugly.
+	dat := coll.UserData.([2]float)
+	// The u and v in intersection code are actually v and w
+	v, w := dat[0], dat[1]
+	u := 1.0 - v - w
+
+	if tri.mesh.normals != nil {
+		na, nb, nc := tri.getNormals()
+		sp.Normal = vector.Add(vector.ScalarMul(na, u), vector.ScalarMul(nb, v), vector.ScalarMul(nc, w)).Normalize()
+	} else {
+		sp.Normal = tri.normal
+	}
+
+	if tri.mesh.hasOrco {
+		// TODO
+	} else {
+		sp.OrcoPosition = coll.GetPoint()
+		sp.HasOrco = false
+		sp.OrcoNormal = sp.GeometricNormal
+	}
+	// TODO if mesh.hasUV
+	// else...
+	{
+		a, b, c := tri.getVertices()
+		sp.U, sp.V = u, v
+		sp.WorldU, sp.WorldV = vector.Sub(b, a), vector.Sub(c, a)
+	}
+
+	sp.Object = tri.mesh
+	sp.Primitive = tri
+	sp.Light = tri.mesh.light
+	sp.Material = tri.material
+
+	sp.SurfaceU, sp.SurfaceV = u, v
+	sp.PrimitiveNumber = tri.index
+	sp.Position = coll.GetPoint()
+
+	sp.NormalU, sp.NormalV = vector.CreateCS(sp.Normal)
+	sp.ShadingU.X = vector.Dot(sp.NormalU, sp.WorldU)
+	sp.ShadingU.Y = vector.Dot(sp.NormalV, sp.WorldU)
+	sp.ShadingU.Z = vector.Dot(sp.Normal, sp.WorldU)
+	sp.ShadingV.X = vector.Dot(sp.NormalU, sp.WorldV)
+	sp.ShadingV.Y = vector.Dot(sp.NormalV, sp.WorldV)
+	sp.ShadingV.Z = vector.Dot(sp.Normal, sp.WorldV)
+
 	return
 }
 
