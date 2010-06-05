12b9e338832546c9159a8931891b64e0bc5fbdc9
Ross Light <ross@zombiezen.com>
2010-06-05 12:32:10 -0700

    Fixed cube creation

--- src/main.go
+++ src/main.go
@@ -19,11 +19,10 @@ import (
 import (
     "./buildversion"
 	"./goray/camera"
-	"./goray/object"
-	"./goray/primitive"
 	"./goray/scene"
 	"./goray/vector"
     "./goray/version"
+    "./goray/std/objects/mesh"
 	trivialInt "./goray/std/integrators/trivial"
 )
 
@@ -102,8 +101,28 @@ func main() {
 	// We should be doing this:
 	//ok := parseXMLFile(f, scene)
 	// For now, we'll do this:
+    cube := mesh.New(12)
+    cube.SetData([]vector.Vector3D{
+        vector.New(-0.5, -0.5, -0.5),
+        vector.New( 0.5, -0.5, -0.5),
+        vector.New( 0.5,  0.5, -0.5),
+        vector.New(-0.5,  0.5, -0.5),
+        
+        vector.New(-0.5, -0.5,  0.5),
+        vector.New( 0.5, -0.5,  0.5),
+        vector.New( 0.5,  0.5,  0.5),
+        vector.New(-0.5,  0.5,  0.5),
+    }, nil)
+    cube.AddTriangle(mesh.NewTriangle(0, 3, 2, cube))
+    cube.AddTriangle(mesh.NewTriangle(0, 2, 1, cube))
+    cube.AddTriangle(mesh.NewTriangle(4, 6, 7, cube))
+    cube.AddTriangle(mesh.NewTriangle(5, 6, 4, cube))
+    for _, p := range cube.GetPrimitives() {
+        fmt.Println(p)
+    }
+    
 	sc.SetCamera(camera.NewOrtho(vector.New(0.0, 0.0, 10.0), vector.New(0.0, 0.0, 0.0), vector.New(0.0, 1.0, 10.0), *width, *height, 1.0, 2.0))
-	sc.AddObject(object.NewPrimitive(primitive.NewSphere(vector.New(0.0, 0.0, 0.0), 1.0, nil)))
+	sc.AddObject(cube)
 	sc.SetSurfaceIntegrator(trivialInt.New())
 
 	fmt.Println("Rendering...")
