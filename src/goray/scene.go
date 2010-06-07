9a9cb6f7721dea9863db35965ffb208f50154ca6
Ross Light <ross@zombiezen.com>
2010-06-06 22:02:46 -0700

    Changed scene to use kd-tree (not working yet)

--- src/goray/scene.go
+++ src/goray/scene.go
@@ -311,7 +311,7 @@ func (s *Scene) Update() (err os.Error) {
 		// Do tree building
 		if len(prims) > 0 {
 			//s.tree = kdtree.New(prims, -1, 1, 0.8, 0.33)
-			s.tree = partition.NewSimple(prims)
+			s.tree = partition.NewKD(prims)
 			s.sceneBound = s.tree.GetBound()
 		}
 	}
