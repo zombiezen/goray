d2f9c74e69df8ff5b80d0907ce67660a28ba7dc9
Ross Light <ross@zombiezen.com>
2010-06-16 16:53:50 -0700

    Added a point light to the scene

--- src/main.go
+++ src/main.go
@@ -29,6 +29,7 @@ import (
 	"goray/core/vector"
 	"goray/core/version"
 	"goray/std/integrators/directlight"
+	pointLight "goray/std/lights/point"
 	debugMaterial "goray/std/materials/debug"
 	"goray/std/objects/mesh"
 	"goray/std/primitives/sphere"
@@ -161,6 +162,7 @@ func main() {
 	}
 
 	sc.SetCamera(camera.NewOrtho(vector.New(5.0, 5.0, 5.0), vector.New(0.0, 0.0, 0.0), vector.New(5.0, 6.0, 5.0), *width, *height, 1.0, 3.0))
+	sc.AddLight(pointLight.New(vector.New(0.0, 5.0, 0.0), color.NewRGB(1.0, 1.0, 1.0), 1.0))
 	sc.AddObject(cube)
 	sc.AddObject(object.PrimitiveObject{sphere.New(vector.New(1, 0, 1), 0.5, mat)})
 
