7b8d1c1cf6a49b4b066b61e349c4076d19d56a05
Ross Light <ross@zombiezen.com>
2010-06-21 19:03:54 -0700

    Exploded kdtree package into multiple files

--- src/goray/core/kdtree/_test.go
+++ src/goray/core/kdtree/_test.go
@@ -3,7 +3,6 @@ package kdtree
 import "testing"
 
 import (
-	"goray/fmath"
 	"goray/core/bound"
 	"goray/core/vector"
 )
@@ -33,42 +32,6 @@ func newBoxTree(boxes []*bound.Bound) *Tree {
 		vals[i] = b
 	}
 	return New(vals, dim, nil)
-}
-
-func TestLeaf(t *testing.T) {
-	myLeaf := newLeaf([]Value{1, 2, 3})
-	if !myLeaf.IsLeaf() {
-		t.Error("Leaf nodes claim that they are not leaves")
-	}
-	vals := myLeaf.GetValues()
-	if len(vals) != 3 {
-		t.Error("Leaf nodes don't store the right number of values")
-	}
-	for i := 0; i < 3; i++ {
-		if vals[i].(int) != i+1 {
-			t.Errorf("Leaf value %d is corrupted", i)
-		}
-	}
-}
-
-func TestInterior(t *testing.T) {
-	leafA, leafB := newLeaf([]Value{}), newLeaf([]Value{})
-	myInt := newInterior(2, 3.14, leafA, leafB)
-	if myInt.IsLeaf() {
-		t.Error("Interior node claims that it is a leaf")
-	}
-	if myInt.GetAxis() != 2 {
-		t.Error("Interior node stores wrong axis")
-	}
-	if !fmath.Eq(myInt.GetPivot(), 3.14) {
-		t.Error("Interior node stores wrong pivot")
-	}
-	if child, _ := myInt.GetLeft().(*Leaf); child != leafA {
-		t.Error("Left child not stored")
-	}
-	if child, _ := myInt.GetRight().(*Leaf); child != leafB {
-		t.Error("Right child not stored")
-	}
 }
 
 func TestLeafTree(t *testing.T) {
