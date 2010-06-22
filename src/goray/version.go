306121f148c562bacdb8438e2c291eb71cf2addf
Ross Light <ross@zombiezen.com>
2010-06-22 15:03:40 -0700

    Moved version package to goray namespace

--- src/goray/version.go
+++ src/goray/version.go
@@ -1,5 +1,5 @@
 //
-//  goray/core/version.go
+//  goray/version.go
 //  goray
 //
 //  Created by Ross Light on 2010-06-04.
@@ -9,8 +9,10 @@ package version
 
 import "fmt"
 
-const Major = 0
-const Minor = 1
-const Patch = 0
+const (
+	Major = 0
+	Minor = 1
+	Patch = 0
+)
 
 func GetString() string { return fmt.Sprintf("%d.%d.%d", Major, Minor, Patch) }
