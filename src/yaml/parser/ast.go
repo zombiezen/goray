f3665d05831c9015c75f5c51f914efc787dce472
Ross Light <ross@zombiezen.com>
2010-07-02 16:28:29 -0700

    Started parser package

---
+++ src/yaml/parser/ast.go
@@ -0,0 +1,60 @@
+//
+//	yaml/parser/ast.go
+//	goray
+//
+//	Created by Ross Light on 2010-07-02.
+//
+
+package parser
+
+import (
+	"yaml/token"
+)
+
+type Stream struct {
+	Documents []Document
+}
+
+type Document struct {
+	MajorVersion int
+	MinorVersion int
+	Content      Node
+}
+
+type Node interface {
+	Start() token.Position
+	Tag() string
+}
+
+type basicNode struct {
+	pos token.Position
+	tag string
+}
+
+func (n basicNode) Start() token.Position { return n.pos }
+func (n basicNode) Tag() string           { return n.tag }
+
+type KeyValuePair struct {
+	Key, Value Node
+}
+
+type Mapping struct {
+	basicNode
+	Pairs []KeyValuePair
+}
+
+type Sequence struct {
+	basicNode
+	Nodes []Node
+}
+
+func (seq *Sequence) At(i int) Node {
+	return seq.Nodes[i]
+}
+
+type Scalar struct {
+	basicNode
+	Value string
+}
+
+func (s *Scalar) String() string { return s.Value }
