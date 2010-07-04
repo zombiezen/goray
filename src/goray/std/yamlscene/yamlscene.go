54a8c638bb35586efe5d8d7f74ad8b453b9805f7
Ross Light <ross@zombiezen.com>
2010-07-04 13:14:22 -0700

    Updated yamlscene for new YAML API

--- src/goray/std/yamlscene/yamlscene.go
+++ src/goray/std/yamlscene/yamlscene.go
@@ -13,30 +13,31 @@ import (
 	"goray/core/color"
 	"goray/core/integrator"
 	"goray/core/scene"
+	yamlData "yaml/data"
 	"yaml/parser"
 )
 
 const (
-	Prefix = "tag:goray/"
+	Prefix    = "tag:goray/"
 	StdPrefix = Prefix + "std/"
 )
 
-var Constructor parser.ConstructorMap
+var Constructor yamlData.ConstructorMap
 
 func init() {
-	Constructor = make(parser.ConstructorMap)
-	Constructor[Prefix + "rgb"] = parser.ConstructorFunc(constructRGB)
-	Constructor[Prefix + "rgba"] = parser.ConstructorFunc(constructRGBA)
+	Constructor = make(yamlData.ConstructorMap)
+	Constructor[Prefix+"rgb"] = yamlData.ConstructorFunc(constructRGB)
+	Constructor[Prefix+"rgba"] = yamlData.ConstructorFunc(constructRGBA)
 }
 
 func Load(r io.Reader, sc *scene.Scene) (i integrator.Integrator, err os.Error) {
 	// Parse
-	p := parser.New(r, nil, parser.ConstructorFunc(realConstructor))
+	p := parser.New(r, yamlData.CoreSchema, yamlData.ConstructorFunc(realConstructor))
 	doc, err := p.ParseDocument()
 	if err != nil {
 		return
 	}
-	
+
 	// Set up scene!
 	root := doc.Content.Data().(map[interface{}]interface{})
 	i = root["integrator"].(integrator.Integrator)
@@ -47,49 +48,41 @@ func realConstructor(n parser.Node) (interface{}, os.Error) {
 	if _, ok := Constructor[n.Tag()]; ok {
 		return Constructor.Construct(n)
 	}
-	return parser.DefaultConstructor.Construct(n)
+	return yamlData.DefaultConstructor.Construct(n)
+}
+
+func floatSequence(n parser.Node) (data []float, ok bool) {
+	seq, ok := n.(*parser.Sequence)
+	if !ok {
+		return
+	}
+	
+	data = make([]float, seq.Len())
+	for i := 0; i < seq.Len(); i++ {
+		var f float64
+		f, ok = yamlData.AsFloat(seq.At(i).Data())
+		if !ok {
+			return
+		}
+		data[i] = float(f)
+	}
+	return
 }
 
 func constructRGB(n parser.Node) (data interface{}, err os.Error) {
-	if node, ok := n.(*parser.Sequence); ok {
-		if node.Len() == 3 {
-			rComp, rOk := parser.GetNodeFloat(node.At(0))
-			gComp, gOk := parser.GetNodeFloat(node.At(1))
-			bComp, bOk := parser.GetNodeFloat(node.At(2))
-			if rOk && gOk && bOk {
-				data = color.NewRGB(rComp, gComp, bComp)
-			} else {
-				err = os.NewError("RGB must have 3 floats")
-			}
-		} else {
-			err = os.NewError("RGB must have 3 components")
-		}
-	} else {
-		err = os.NewError("RGB must be a sequence")
+	comps, ok := floatSequence(n)
+	if !ok || len(comps) != 3 {
+		err = os.NewError("RGB must be a sequence of 3 floats")
+		return
 	}
-	
-	return
+	return color.NewRGB(comps[0], comps[1], comps[2]), nil
 }
 
 func constructRGBA(n parser.Node) (data interface{}, err os.Error) {
-	if node, ok := n.(*parser.Sequence); ok {
-		if node.Len() == 4 {
-			rComp, rOk := parser.GetNodeFloat(node.At(0))
-			gComp, gOk := parser.GetNodeFloat(node.At(1))
-			bComp, bOk := parser.GetNodeFloat(node.At(2))
-			aComp, aOk := parser.GetNodeFloat(node.At(3))
-			
-			if rOk && gOk && bOk && aOk {
-				data = color.NewRGBA(rComp, gComp, bComp, aComp)
-			} else {
-				err = os.NewError("RGBA must have 4 floats")
-			}
-		} else {
-			err = os.NewError("RGBA must have 4 components")
-		}
-	} else {
-		err = os.NewError("RGBA must be a sequence")
+	comps, ok := floatSequence(n)
+	if !ok || len(comps) != 4 {
+		err = os.NewError("RGBA must be a sequence of 4 floats")
+		return
 	}
-	
-	return
+	return color.NewRGBA(comps[0], comps[1], comps[2], comps[3]), nil
 }
