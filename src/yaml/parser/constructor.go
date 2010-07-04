15ca8b025f98628c3d976fd1f12986ce89037949
Ross Light <ross@zombiezen.com>
2010-07-04 10:16:44 -0700

    Fixed logic error regarding nulls

--- src/yaml/parser/constructor.go
+++ src/yaml/parser/constructor.go
@@ -90,7 +90,7 @@ func constructMap(n Node) (data interface{}, err os.Error) {
 func constructNull(n Node) (data interface{}, err os.Error) {
 	_, isScalar := n.(*Scalar)
 	_, isEmpty := n.(*Empty)
-	if !isScalar || !isEmpty {
+	if !isScalar && !isEmpty {
 		err = os.NewError("Non-scalar tagged as null")
 		return
 	}
