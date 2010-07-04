08e2f86912e850afbc0afc9cf5772910a903080a
Ross Light <ross@zombiezen.com>
2010-07-04 09:23:02 -0700

    Added core schema and constructors

--- src/yaml/parser/constructor.go
+++ src/yaml/parser/constructor.go
@@ -8,7 +8,9 @@
 package parser
 
 import (
+	"math"
 	"os"
+	"strconv"
 )
 
 func init() {
@@ -16,6 +18,10 @@ func init() {
 	m[StringTag] = ConstructorFunc(constructStr)
 	m[SequenceTag] = ConstructorFunc(constructSeq)
 	m[MappingTag] = ConstructorFunc(constructMap)
+	m[NullTag] = ConstructorFunc(constructNull)
+	m[BoolTag] = ConstructorFunc(constructBool)
+	m[IntTag] = ConstructorFunc(constructInt)
+	m[FloatTag] = ConstructorFunc(constructFloat)
 	DefaultConstructor = m
 }
 
@@ -78,5 +84,85 @@ func constructMap(n Node) (data interface{}, err os.Error) {
 		m[k] = v
 	}
 	data = m
+	return
+}
+
+func constructNull(n Node) (data interface{}, err os.Error) {
+	if _, ok := n.(*Scalar); !ok {
+		err = os.NewError("Non-scalar tagged as null")
+		return
+	}
+	return nil, nil
+}
+
+func constructBool(n Node) (data interface{}, err os.Error) {
+	var s string
+
+	if scalar, ok := n.(*Scalar); ok {
+		s = scalar.Value
+	} else {
+		err = os.NewError("Non-scalar tagged as bool")
+		return
+	}
+	groups := csBoolPat.MatchStrings(s)
+
+	if len(groups) > 0 {
+		if groups[1] != "" {
+			return true, nil
+		} else if groups[2] != "" {
+			return false, nil
+		}
+	}
+
+	err = os.NewError("Value is an invalid boolean: " + s)
+	return
+}
+
+func constructInt(n Node) (data interface{}, err os.Error) {
+	var s string
+
+	if scalar, ok := n.(*Scalar); ok {
+		s = scalar.Value
+	} else {
+		err = os.NewError("Non-scalar tagged as int")
+		return
+	}
+
+	if csDecPat.MatchString(s) {
+		return strconv.Atoi64(s)
+	} else if groups := csHexPat.MatchStrings(s); len(groups) > 0 {
+		return strconv.Btoui64(groups[1], 16)
+	} else if groups := csOctPat.MatchStrings(s); len(groups) > 0 {
+		return strconv.Btoui64(groups[1], 8)
+	}
+
+	err = os.NewError("Value is an invalid int: " + s)
+	return
+}
+
+func constructFloat(n Node) (data interface{}, err os.Error) {
+	var s string
+
+	if scalar, ok := n.(*Scalar); ok {
+		s = scalar.Value
+	} else {
+		err = os.NewError("Non-scalar tagged as float")
+		return
+	}
+
+	switch {
+	case csFloatPat.MatchString(s):
+		return strconv.Atof64(s)
+	case csInfPat.MatchString(s):
+		sign := 1
+		if s[0] == '-' {
+			sign = -1
+		}
+		return math.Inf(sign), nil
+	case csNanPat.MatchString(s):
+		return math.NaN(), nil
+	}
+
+	err = os.NewError("Value is an invalid float: " + s)
 	return
 }
--- src/yaml/parser/schema.go
+++ src/yaml/parser/schema.go
@@ -9,6 +9,7 @@ package parser
 
 import (
 	"os"
+	"regexp"
 )
 
 const (
@@ -34,6 +35,7 @@ func (f SchemaFunc) Resolve(n Node) (string, os.Error) { return f(n) }
 
 var (
 	FailsafeSchema Schema = SchemaFunc(failsafeSchema)
+	CoreSchema     = SchemaFunc(coreSchema)
 )
 
 func failsafeSchema(node Node) (tag string, err os.Error) {
@@ -48,4 +50,35 @@ func failsafeSchema(node Node) (tag string, err os.Error) {
 		err = os.NewError("Unrecognized node given to failsafe schema")
 	}
 	return
+}
+
+var (
+	csNullPat  = regexp.MustCompile(`null|Null|NULL|~`)
+	csBoolPat  = regexp.MustCompile(`(true|True|TRUE)|(false|False|FALSE)`)
+	csDecPat   = regexp.MustCompile(`([-+]?)([0-9]+)`)
+	csOctPat   = regexp.MustCompile(`0o([0-7]+)`)
+	csHexPat   = regexp.MustCompile(`0x([0-9a-fA-F]+)`)
+	csFloatPat = regexp.MustCompile(`([-+]?)(\.[0-9]+|[0-9]+(\.[0-9]*)?)([eE][-+]?[0-9]+)?`)
+	csInfPat   = regexp.MustCompile(`([-+]?)\.(inf|Inf|INF)`)
+	csNanPat   = regexp.MustCompile(`\.(nan|NaN|NAN)`)
+)
+
+func coreSchema(node Node) (tag string, err os.Error) {
+	if scalar, ok := node.(*Scalar); ok {
+		s := scalar.String()
+		switch {
+		case csNullPat.MatchString(s):
+			return NullTag, nil
+		case csBoolPat.MatchString(s):
+			return BoolTag, nil
+		case csDecPat.MatchString(s) || csOctPat.MatchString(s) || csHexPat.MatchString(s):
+			return IntTag, nil
+		case csFloatPat.MatchString(s) || csInfPat.MatchString(s) || csNanPat.MatchString(s):
+			return FloatTag, nil
+		default:
+			return StringTag, nil
+		}
+	}
+
+	return failsafeSchema(node)
 }
