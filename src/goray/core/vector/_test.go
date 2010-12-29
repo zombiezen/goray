575a57bc5d107b86cbd82512bc36a5156bfdc86a
Ross Light <ross@zombiezen.com>
2010-12-29 11:25:54 -0800

    Change Vector3D to array of float64

--- src/goray/core/vector/_test.go
+++ src/goray/core/vector/_test.go
@@ -3,96 +3,92 @@ package vector
 import "testing"
 import "goray/fmath"
 
-func TestNew(t *testing.T) {
-	comps := []float{1.5, -5.7, 4.0}
-	v := New(comps[0], comps[1], comps[2])
-
-	if !fmath.Eq(v.X, comps[0]) {
-		t.Error("X component does not match")
-	}
-	if !fmath.Eq(v.Y, comps[1]) {
-		t.Error("Y component does not match")
-	}
-	if !fmath.Eq(v.Z, comps[2]) {
-		t.Error("Z component does not match")
-	}
-}
-
 func TestNormalize(t *testing.T) {
-	comps := []float{1.0, 2.0, -2.0}
+	comps := []float64{1.0, 2.0, -2.0}
 	length := 3.0
-	v := New(comps[0], comps[1], comps[2])
+	v := Vector3D{comps[0], comps[1], comps[2]}
 	vn := v.Normalize()
 	if vn.Length() != 1.0 {
 		t.Error("Length is not 1")
 	}
-	if vn.X != comps[0]/length {
-		t.Error("X component is incorrect")
-	}
-	if vn.Y != comps[1]/length {
-		t.Error("Y component is incorrect")
-	}
-	if vn.Z != comps[2]/length {
-		t.Error("Z component is incorrect")
+	for axis := X; axis <= Z; axis++ {
+		if vn[axis] != comps[axis]/length {
+			t.Error("X component is incorrect")
+		}
 	}
 }
 
 func TestLength(t *testing.T) {
 	type lengthTest struct {
-		Comps  [3]float
-		Length float
+		Vec    Vector3D
+		Length float64
 	}
 
 	tests := []lengthTest{
-		lengthTest{[3]float{0.0, 0.0, 0.0}, 0.0},
+		lengthTest{[3]float64{0.0, 0.0, 0.0}, 0.0},
 
-		lengthTest{[3]float{1.0, 0.0, 0.0}, 1.0},
-		lengthTest{[3]float{0.0, 1.0, 0.0}, 1.0},
-		lengthTest{[3]float{0.0, 0.0, 1.0}, 1.0},
+		lengthTest{[3]float64{1.0, 0.0, 0.0}, 1.0},
+		lengthTest{[3]float64{0.0, 1.0, 0.0}, 1.0},
+		lengthTest{[3]float64{0.0, 0.0, 1.0}, 1.0},
 
-		lengthTest{[3]float{-1.0, 0.0, 0.0}, 1.0},
-		lengthTest{[3]float{0.0, -1.0, 0.0}, 1.0},
-		lengthTest{[3]float{0.0, 0.0, -1.0}, 1.0},
+		lengthTest{[3]float64{-1.0, 0.0, 0.0}, 1.0},
+		lengthTest{[3]float64{0.0, -1.0, 0.0}, 1.0},
+		lengthTest{[3]float64{0.0, 0.0, -1.0}, 1.0},
 
-		lengthTest{[3]float{3.0, -4.0, 0.0}, 5.0},
-		lengthTest{[3]float{1.0, 2.0, -2.0}, 3.0},
-		lengthTest{[3]float{3.14, 20.7, 0.5}, 20.942769635365803},
+		lengthTest{[3]float64{3.0, -4.0, 0.0}, 5.0},
+		lengthTest{[3]float64{1.0, 2.0, -2.0}, 3.0},
+		lengthTest{[3]float64{3.14, 20.7, 0.5}, 20.942769635365803},
 	}
 
 	for _, ltest := range tests {
-		v := New(ltest.Comps[0], ltest.Comps[1], ltest.Comps[2])
-		if lensqr := ltest.Length * ltest.Length; !fmath.Eq(v.LengthSqr(), lensqr) {
-			t.Error("LengthSqr failed for %v (wanted %.2f, got %.2f)", v, lensqr, v.LengthSqr())
+		if lensqr := ltest.Length * ltest.Length; ltest.Vec.LengthSqr() != lensqr {
+			t.Error("LengthSqr failed for %v (wanted %.2f, got %.2f)", ltest.Vec, lensqr, ltest.Vec.LengthSqr())
 		}
-		if !fmath.Eq(v.Length(), ltest.Length) {
-			t.Error("Length failed for %v (wanted %.2f, got %.2f)", v, ltest.Length, v.Length())
+		if ltest.Vec.Length() != ltest.Length {
+			t.Error("Length failed for %v (wanted %.2f, got %.2f)", ltest.Vec, ltest.Length, ltest.Vec.Length())
 		}
 	}
 }
 
 func TestAbs(t *testing.T) {
-	if v := New(0, 0, 0).Abs(); v.X != 0 || v.Y != 0 || v.Z != 0 {
+	var v Vector3D
+
+	v = Vector3D{0, 0, 0}.Abs()
+	if v[X] != 0 || v[Y] != 0 || v[Z] != 0 {
 		t.Error("Zero vector incorrect")
 	}
-	if v := New(1, 2, 3).Abs(); v.X != 1 || v.Y != 2 || v.Z != 3 {
+
+	v = Vector3D{1, 2, 3}.Abs()
+	if v[X] != 1 || v[Y] != 2 || v[Z] != 3 {
 		t.Error("All positive vector incorrect")
 	}
-	if v := New(-1, -2, -3).Abs(); v.X != 1 || v.Y != 2 || v.Z != 3 {
+
+	v = Vector3D{-1, -2, -3}.Abs()
+	if v[X] != 1 || v[Y] != 2 || v[Z] != 3 {
 		t.Error("All negative vector incorrect")
 	}
-	if v := New(-1, 2, -3).Abs(); v.X != 1 || v.Y != 2 || v.Z != 3 {
+
+	v = Vector3D{-1, 2, -3}.Abs()
+	if v[X] != 1 || v[Y] != 2 || v[Z] != 3 {
 		t.Error("Mixed vector incorrect")
 	}
 }
 
 func TestIsZero(t *testing.T) {
-	if v := New(0, 0, 0); !v.IsZero() {
+	var v Vector3D
+
+	v = Vector3D{0, 0, 0}
+	if !v.IsZero() {
 		t.Error("Zero vector is not zero")
 	}
-	if v := New(1, 0, 0); v.IsZero() {
+
+	v = Vector3D{1, 0, 0}
+	if v.IsZero() {
 		t.Error("Positive X vector is zero")
 	}
-	if v := New(-1, 0, 0); v.IsZero() {
+
+	v = Vector3D{-1, 0, 0}
+	if v.IsZero() {
 		t.Error("Negative X vector is zero")
 	}
 }
--- src/goray/core/vector/vector.go
+++ src/goray/core/vector/vector.go
@@ -8,17 +8,21 @@
 // The vector package provides a three-dimensional vector type and various operations on it.
 package vector
 
-import "fmt"
-import "goray/fmath"
+import (
+	"fmt"
+	"math"
+)
 
-// Vector3D represents a three-dimensional vector.
+// Axes
+const (
+	X = iota
+	Y
+	Z
+)
+
+// Vector3D represents a three-dimensional vector. The default value is a zero vector.
 // The old Yafaray engine had a distinct type for a point and a normal, but we just represent everything as vectors.
-type Vector3D struct {
-	X, Y, Z float
-}
-
-// New creates a new vector.
-func New(x, y, z float) Vector3D { return Vector3D{x, y, z} }
+type Vector3D [3]float64
 
 // Normalize creates a new vector that is of unit length in the same direction as the vector.
 func (v Vector3D) Normalize() Vector3D {
@@ -30,128 +34,111 @@ func (v Vector3D) Normalize() Vector3D {
 }
 
 // Length returns the magnitude of the vector.
-func (v Vector3D) Length() float { return fmath.Sqrt(v.LengthSqr()) }
+func (v Vector3D) Length() float64 { return math.Sqrt(v.LengthSqr()) }
 
 // LengthSqr returns the magnitude squared of the vector.  This is cheaper to compute than Length.
-func (v Vector3D) LengthSqr() float { return v.X*v.X + v.Y*v.Y + v.Z*v.Z }
+func (v Vector3D) LengthSqr() float64 { return v[X]*v[X] + v[Y]*v[Y] + v[Z]*v[Z] }
 
 // Abs returns a new vector with all positive components.
-func (v Vector3D) Abs() Vector3D { return Vector3D{fmath.Abs(v.X), fmath.Abs(v.Y), fmath.Abs(v.Z)} }
+func (v Vector3D) Abs() Vector3D { return Vector3D{math.Fabs(v[X]), math.Fabs(v[Y]), math.Fabs(v[Z])} }
 
 // Inverse returns a new vector that is the result of 1.0 / v.GetComponent(i) for all i.  Any zero value is left as zero.
 func (v Vector3D) Inverse() (r Vector3D) {
-	if !fmath.Eq(v.X, 0.0) {
-		r.X = 1.0 / v.X
-	}
-	if !fmath.Eq(v.Y, 0.0) {
-		r.Y = 1.0 / v.Y
-	}
-	if !fmath.Eq(v.Z, 0.0) {
-		r.Z = 1.0 / v.Z
+	for axis, comp := range v {
+		if comp != 0.0 {
+			r[axis] = 1.0 / comp
+		}
 	}
 	return
 }
 
 // IsZero indicates whether the vector is the zero vector.
-func (v Vector3D) IsZero() bool { return v.X == 0 && v.Y == 0 && v.Z == 0 }
-
-// GetComponent returns one of the vector's components by index.
-func (v Vector3D) GetComponent(i int) float {
-	switch i {
-	case 0:
-		return v.X
-	case 1:
-		return v.Y
-	case 2:
-		return v.Z
-	}
-	return 0.0
-}
+func (v Vector3D) IsZero() bool { return v[X] == 0 && v[Y] == 0 && v[Z] == 0 }
 
 func (v Vector3D) String() string {
-	return fmt.Sprintf("<%.2f, %.2f, %.2f>", v.X, v.Y, v.Z)
+	return fmt.Sprintf("<%.2f, %.2f, %.2f>", v[X], v[Y], v[Z])
 }
 
 // Add computes the sum of two or more vectors.
 func Add(v1, v2 Vector3D, vn ...Vector3D) Vector3D {
-	result := Vector3D{v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z}
-	for i := 0; i < len(vn); i++ {
-		result.X += vn[i].X
-		result.Y += vn[i].Y
-		result.Z += vn[i].Z
+	result := Vector3D{v1[X] + v2[X], v1[Y] + v2[Y], v1[Z] + v2[Z]}
+	for _, u := range vn {
+		result[X] += u[X]
+		result[Y] += u[Y]
+		result[Z] += u[Z]
 	}
 	return result
 }
 
 // ScalarAdd adds a scalar to all of a vector's components.
-func ScalarAdd(v Vector3D, s float) Vector3D {
-	return Vector3D{v.X + s, v.Y + s, v.Z + s}
+func ScalarAdd(v Vector3D, s float64) Vector3D {
+	return Vector3D{v[X] + s, v[Y] + s, v[Z] + s}
 }
 
 // Sub computes the difference of two or more vectors.
 func Sub(v1, v2 Vector3D, vn ...Vector3D) Vector3D {
-	result := Vector3D{v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z}
-	for i := 0; i < len(vn); i++ {
-		result.X -= vn[i].X
-		result.Y -= vn[i].Y
-		result.Z -= vn[i].Z
+	result := Vector3D{v1[X] - v2[X], v1[Y] - v2[Y], v1[Z] - v2[Z]}
+	for _, u := range vn {
+		result[X] -= u[X]
+		result[Y] -= u[Y]
+		result[Z] -= u[Z]
 	}
 	return result
 }
 
 // ScalarSub subtracts a scalar from all of a vector's components.
-func ScalarSub(v Vector3D, s float) Vector3D {
-	return Vector3D{v.X - s, v.Y - s, v.Z - s}
+func ScalarSub(v Vector3D, s float64) Vector3D {
+	return Vector3D{v[X] - s, v[Y] - s, v[Z] - s}
 }
 
 // ScalarMul multiplies all of a vector's components by a scalar.
-func ScalarMul(v Vector3D, s float) Vector3D {
-	return Vector3D{v.X * s, v.Y * s, v.Z * s}
+func ScalarMul(v Vector3D, s float64) Vector3D {
+	return Vector3D{v[X] * s, v[Y] * s, v[Z] * s}
 }
 
 // ScalarMul divides all of a vector's components by a scalar.
-func ScalarDiv(v Vector3D, s float) Vector3D {
-	return Vector3D{v.X / s, v.Y / s, v.Z / s}
+func ScalarDiv(v Vector3D, s float64) Vector3D {
+	return Vector3D{v[X] / s, v[Y] / s, v[Z] / s}
 }
 
 // Dot computes the dot product of two vectors.
-func Dot(v1, v2 Vector3D) float {
-	return v1.X*v2.X + v1.Y*v2.Y + v1.Z*v2.Z
+func Dot(v1, v2 Vector3D) float64 {
+	return v1[X]*v2[X] + v1[Y]*v2[Y] + v1[Z]*v2[Z]
 }
 
 // Cross computes the cross product of two vectors.
 func Cross(v1, v2 Vector3D) Vector3D {
 	return Vector3D{
-		v1.Y*v2.Z - v1.Z*v2.Y,
-		v1.Z*v2.X - v1.X*v2.Z,
-		v1.X*v2.Y - v1.Y*v2.X,
+		v1[Y]*v2[Z] - v1[Z]*v2[Y],
+		v1[Z]*v2[X] - v1[X]*v2[Z],
+		v1[X]*v2[Y] - v1[Y]*v2[X],
 	}
 }
 
 // CompMul multiplies the components of two vectors together.
 func CompMul(v1, v2 Vector3D) Vector3D {
-	return Vector3D{v1.X * v2.X, v1.Y * v2.Y, v1.Z * v2.Z}
+	return Vector3D{v1[X] * v2[X], v1[Y] * v2[Y], v1[Z] * v2[Z]}
 }
 
 // CompDiv divides the components of two vectors.
 func CompDiv(v1, v2 Vector3D) Vector3D {
-	return Vector3D{v1.X / v2.X, v1.Y / v2.Y, v1.Z / v2.Z}
+	return Vector3D{v1[X] / v2[X], v1[Y] / v2[Y], v1[Z] / v2[Z]}
 }
 
 // CreateCS finds two normalized vectors orthogonal to the given one that can be used as a coordinate system.
 //
 // This is particularly useful for UV-mapping and the like.
 func CreateCS(normal Vector3D) (u, v Vector3D) {
-	if normal.X == 0 && normal.Y == 0 {
-		if normal.Z < 0 {
-			u = New(-1.0, 0.0, 0.0)
+	if normal[X] == 0 && normal[Y] == 0 {
+		if normal[Z] < 0 {
+			u = Vector3D{-1.0, 0.0, 0.0}
 		} else {
-			u = New(1.0, 0.0, 0.0)
+			u = Vector3D{1.0, 0.0, 0.0}
 		}
-		v = New(0.0, 1.0, 0.0)
+		v = Vector3D{0.0, 1.0, 0.0}
 	} else {
-		d := 1.0 / fmath.Sqrt(normal.Y*normal.Y+normal.X*normal.X)
-		u = New(normal.Y*d, -normal.X*d, 0.0)
+		d := 1.0 / math.Sqrt(normal[Y]*normal[Y]+normal[X]*normal[X])
+		u = Vector3D{normal[Y] * d, -normal[X] * d, 0.0}
 		v = Cross(normal, u)
 	}
 	return
