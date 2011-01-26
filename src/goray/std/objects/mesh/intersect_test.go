64cd16a9fd0a732843ca9b32bc6be916cd2c6fc2
Ross Light <ross@zombiezen.com>
2011-01-26 00:22:27 -0800

    Add other axis test cases (causes assembly routine to crash)

--- src/goray/std/objects/mesh/intersect_test.go
+++ src/goray/std/objects/mesh/intersect_test.go
@@ -27,11 +27,35 @@ func TestIntersect(t *testing.T) {
 			U: 0.0,
 			V: 0.0,
 		},
+		{
+			Vertices: [3][3]float64{
+				{0.0, 1.0, 0.0},
+				{1.0, 1.0, 0.0},
+				{0.0, 1.0, 1.0},
+			},
+			RayDir: [3]float64{0.0, 1.0, 0.0},
+			RayFrom: [3]float64{0.0, 0.0, 0.0},
+			RayDepth: 1.0,
+			U: 0.0,
+			V: 0.0,
+		},
+		{
+			Vertices: [3][3]float64{
+				{0.0, 0.0, 1.0},
+				{0.0, 1.0, 1.0},
+				{1.0, 0.0, 1.0},
+			},
+			RayDir: [3]float64{0.0, 0.0, 1.0},
+			RayFrom: [3]float64{0.0, 0.0, 0.0},
+			RayDepth: 1.0,
+			U: 0.0,
+			V: 0.0,
+		},
 	}
-	for _, itest := range testList {
+	for i, itest := range testList {
 		rayDepth, u, v := intersect(itest.Vertices[0], itest.Vertices[1], itest.Vertices[2], itest.RayDir, itest.RayFrom)
 		if rayDepth != itest.RayDepth || u != itest.U || v != itest.V {
-			t.Errorf("intersect() failed, wanted %.3f, %.3f, %.3f but got %.3f, %.3f, %.3f", itest.RayDepth, itest.U, itest.V, rayDepth, u, v)
+			t.Errorf("intersect() [%d] failed, wanted %.3f, %.3f, %.3f but got %.3f, %.3f, %.3f", i, itest.RayDepth, itest.U, itest.V, rayDepth, u, v)
 		}
 	}
 }
