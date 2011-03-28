471c148ab68662a0a13d6ad4f7b9c37874129214
Ross Light <ross@zombiezen.com>
2011-03-28 13:32:24 -0700

    Initial commit of spot light (untested)

--- src/goray/sampleutil.go
+++ src/goray/sampleutil.go
@@ -37,6 +37,15 @@ func Sphere(s1, s2 float64) (dir vector.Vector3D) {
 	return
 }
 
+// Cone uniformly samples a cone.
+func Cone(d, u, v vector.Vector3D, maxCosAngle, s1, s2 float64) vector.Vector3D {
+	cosAngle := 1 - (1-maxCosAngle)*s2
+	sinAngle := math.Sqrt(1 - cosAngle*cosAngle)
+	t1 := 2 * math.Pi * s1
+	// \sin \theta (\vec{u} \cos t_1 + \vec{v} \cos t_1) + \vec{d} \cos \theta
+	return vector.Add(vector.ScalarMul(vector.Add(vector.ScalarMul(u, math.Cos(t1)), vector.ScalarMul(v, math.Sin(t1))), sinAngle), vector.ScalarMul(d, cosAngle))
+}
+
 // AddMod1 performs an floating-point addition modulo 1. Both values must be in the range [0,1].
 func AddMod1(a, b float64) (s float64) {
 	s = a + b
@@ -48,39 +57,41 @@ func AddMod1(a, b float64) (s float64) {
 
 // Pdf1D stores a 1-dimensonal probability distribution function.
 type Pdf1D struct {
-	f, cdf   []float64
-	integral float64
+	F, Cdf   []float64
+	Integral float64
 }
 
 // NewPdf1D creates a new probably distribution function and calculates its
 // cumulative distribution function.
 func NewPdf1D(f []float64) (p Pdf1D) {
-	p.f = f
-	p.cdf = make([]float64, 1, len(f)+1)
-	p.cdf[0] = 0.0
-	delta := 1.0 / float64(len(p.f))
-	for i := range p.f {
-		p.integral += p.f[i] * delta
-		p.cdf = append(p.cdf, p.integral)
+	p.F = f
+	p.Cdf = make([]float64, 1, len(f)+1)
+	p.Cdf[0] = 0.0
+	delta := 1.0 / float64(len(p.F))
+	for i := range p.F {
+		p.Integral += p.F[i] * delta
+		p.Cdf = append(p.Cdf, p.Integral)
 	}
-	for i := range p.cdf {
-		p.cdf[i] /= p.integral
+	for i := range p.Cdf {
+		p.Cdf[i] /= p.Integral
 	}
 	return
 }
 
+func (p Pdf1D) Len() int { return len(p.F) }
+
 func (p Pdf1D) Sample(u float64) (offset, pdf float64) {
-	index := sort.Search(len(p.cdf), func(i int) bool { return p.cdf[i] >= u }) - 1
-	delta := (u - p.cdf[index]) / (p.cdf[index+1] - p.cdf[index])
-	return float64(index) + delta, p.f[index] / p.integral
+	index := sort.Search(len(p.Cdf), func(i int) bool { return p.Cdf[i] >= u }) - 1
+	delta := (u - p.Cdf[index]) / (p.Cdf[index+1] - p.Cdf[index])
+	return float64(index) + delta, p.F[index] / p.Integral
 }
 
 func (p Pdf1D) DiscreteSample(u float64) (index int, pdf float64) {
 	if u == 0 {
-		pdf = p.f[0] / p.integral
+		pdf = p.F[0] / p.Integral
 		return
 	}
-	index = sort.Search(len(p.cdf), func(i int) bool { return p.cdf[i] >= u }) - 1
-	pdf = p.f[index] / p.integral
+	index = sort.Search(len(p.Cdf), func(i int) bool { return p.Cdf[i] >= u }) - 1
+	pdf = p.F[index] / p.Integral
 	return
 }
---
+++ src/goray/std/lights/spot.go
@@ -0,0 +1,191 @@
+//
+//	goray/std/lights/spot.go
+//	goray
+//
+//	Created by Ross Light on 2011-03-23.
+//
+
+package spot
+
+import (
+	"math"
+	"os"
+
+	"goray/core/color"
+	"goray/core/light"
+	"goray/core/ray"
+	"goray/core/surface"
+	"goray/core/vector"
+	"goray/sampleutil"
+
+	yamldata "goyaml.googlecode.com/hg/data"
+)
+
+type spotLight struct {
+	position         vector.Vector3D
+	direction        vector.Vector3D
+	du, dv           vector.Vector3D
+	cosStart, cosEnd float64
+	icosDiff         float64
+	intensity        float64
+	color            color.Color
+	pdf              sampleutil.Pdf1D
+	interv1, interv2 float64
+}
+
+func New(from, to vector.Vector3D, col color.Color, power, angle, falloff float64) light.Light {
+	newSpot := &spotLight{
+		position:  from,
+		direction: vector.Sub(to, from).Normalize(),
+		color:     color.ScalarMul(col, power),
+		intensity: power,
+	}
+	newSpot.du, newSpot.dv = vector.CreateCS(newSpot.direction)
+	radAngle := angle * math.Pi / 180
+	radInnerAngle := radAngle * (1 - falloff)
+	newSpot.cosStart = math.Cos(radInnerAngle)
+	newSpot.cosEnd = math.Cos(radAngle)
+	newSpot.icosDiff = 1.0 / (newSpot.cosStart - newSpot.cosEnd)
+	f := make([]float64, 65)
+	for i := range f {
+		v := float64(i) / float64(len(f))
+		f[i] = v * v * (3 - 2*v)
+	}
+	newSpot.pdf = sampleutil.NewPdf1D(f)
+	/* the integral of the smoothstep is 0.5, and since it gets applied to the cos, and each delta cos
+	corresponds to a constant surface are of the (partial) emitting sphere, we can actually simply
+	compute the energy emitted from both areas, the constant and blending one...
+	1  cosStart  cosEnd              -1
+	|------|--------|-----------------|
+	*/
+	newSpot.interv1 = 1 - newSpot.cosStart
+	newSpot.interv2 = 0.5 * (newSpot.cosStart - newSpot.cosEnd)
+	sum := newSpot.interv1 + newSpot.interv2
+	if sum > 1e-10 {
+		newSpot.interv1 /= sum
+		newSpot.interv2 /= sum
+	}
+	return newSpot
+}
+
+func (spot *spotLight) LightFlags() uint { return light.TypeSingular }
+func (spot *spotLight) NumSamples() int  { return 1 }
+
+func (spot *spotLight) SetScene(scene interface{}) {}
+
+func (spot *spotLight) TotalEnergy() color.Color {
+	return color.ScalarMul(spot.color, 2*math.Pi*(1-0.5*(spot.cosStart+spot.cosEnd)))
+}
+
+func (spot *spotLight) Illuminate(sp surface.Point, wi *ray.Ray) (col color.Color, ok bool) {
+	ldir := vector.Sub(spot.position, sp.Position)
+	distSqr := ldir.LengthSqr()
+	dist := math.Sqrt(distSqr)
+	if dist == 0 {
+		return
+	}
+	ldir = vector.ScalarDiv(ldir, dist) // normalize
+	cosa := vector.Dot(spot.direction.Negate(), ldir)
+	switch {
+	case cosa < spot.cosEnd:
+		// Outside cone
+		return
+	case cosa >= spot.cosStart:
+		// Not affected by falloff
+		col = color.ScalarDiv(spot.color, distSqr)
+	default:
+		v := (cosa - spot.cosEnd) * spot.icosDiff
+		v = v * v * (3 - 2*v)
+		col = color.ScalarMul(spot.color, v/distSqr)
+	}
+	wi.TMax = dist
+	wi.Dir = ldir
+	ok = true
+	return
+}
+
+func (spot *spotLight) IlluminateSample(sp surface.Point, wi *ray.Ray, s *light.Sample) (ok bool) {
+	s.Color, ok = spot.Illuminate(sp, wi)
+	if ok {
+		s.Flags = spot.LightFlags()
+		s.Pdf = vector.Sub(spot.position, sp.Position).LengthSqr()
+	}
+	return
+}
+
+func (spot *spotLight) IlluminatePdf(sp, spLight surface.Point) float64 {
+	return 0
+}
+
+func (spot *spotLight) emit(s1, s2, s3 float64) (col color.Color, wo vector.Vector3D, pdf float64) {
+	col = spot.color
+	if s3 <= spot.interv1 {
+		// Sample from cone not affected by falloff
+		col = spot.color
+		wo = sampleutil.Cone(spot.direction, spot.du, spot.dv, spot.cosStart, s1, s2)
+		pdf = spot.interv1 / (2 * (1 - spot.cosStart))
+		return
+	}
+	sm2, spdf := spot.pdf.Sample(s2)
+	sm2 /= float64(spot.pdf.Len())
+	pdf = (spot.interv2 * spdf) / (2.0 * (spot.cosStart - spot.cosEnd))
+	cosAngle := spot.cosEnd + (spot.cosStart-spot.cosEnd)*sm2
+	sinAngle := math.Sqrt(1 - cosAngle*cosAngle)
+	t1 := 2 * math.Pi * s1
+	wo = vector.Add(vector.ScalarMul(vector.Add(vector.ScalarMul(spot.du, math.Cos(t1)), vector.ScalarMul(spot.dv, math.Sin(t1))), sinAngle), vector.ScalarMul(spot.direction, cosAngle))
+	col = color.ScalarMul(spot.color, spdf*spot.pdf.Integral) // color is scaled by falloff
+	return
+}
+
+func (spot *spotLight) EmitPhoton(s1, s2, s3, s4 float64) (col color.Color, r ray.Ray, ipdf float64) {
+	col, r.Dir, ipdf = spot.emit(s1, s2, s3)
+	ipdf = math.Pi / ipdf
+	r.From = spot.position
+	return
+}
+
+func (spot *spotLight) EmitSample(s *light.Sample) (wo vector.Vector3D, col color.Color) {
+	col, wo, s.DirPdf = spot.emit(s.S1, s.S2, s.S3)
+	s.Point.Position = spot.position
+	s.AreaPdf = 1.0
+	s.Flags = spot.LightFlags()
+	return
+}
+
+func (spot *spotLight) EmitPdf(sp surface.Point, wo vector.Vector3D) (areaPdf, dirPdf, cosWo float64) {
+	areaPdf, cosWo = 1, 1
+	cosa := vector.Dot(spot.direction, wo)
+	switch {
+	case cosa < spot.cosEnd:
+		dirPdf = 0
+	case cosa >= spot.cosStart:
+		dirPdf = spot.interv1 / (2 * (1 - spot.cosStart))
+	default:
+		v := (cosa - spot.cosEnd) * spot.icosDiff
+		v = v * v * (3 - 2*v)
+		dirPdf = spot.interv2 * v / (spot.cosStart - spot.cosEnd)
+	}
+	return
+}
+
+func (spot *spotLight) CanIlluminate(pt vector.Vector3D) bool {
+	ldir := vector.Sub(spot.position, pt)
+	dist := ldir.Length()
+	if dist == 0 {
+		return false
+	}
+	ldir = vector.ScalarDiv(ldir, dist) // normalize
+	cosa := vector.Dot(spot.direction.Negate(), ldir)
+	return cosa >= spot.cosEnd
+}
+
+func Construct(m yamldata.Map) (data interface{}, err os.Error) {
+	pos := m["position"].(vector.Vector3D)
+	look := m["look"].(vector.Vector3D)
+	col := m["color"].(color.Color)
+	power, _ := yamldata.AsFloat(m["intensity"])
+	angle, _ := yamldata.AsFloat(m["coneAngle"])
+	falloff, _ := yamldata.AsFloat(m["falloff"])
+	data = New(pos, look, col, power, angle, falloff)
+	return
+}
