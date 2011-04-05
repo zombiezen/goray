4b9002722fb17308881a3420e847da8f8c4fdd60
Ross Light <ross@zombiezen.com>
2011-04-05 11:49:49 -0700

    gofmt goray/std/shaders/texmap/projection.go

--- src/goray/std/shaders/texmap/projection.go
+++ src/goray/std/shaders/texmap/projection.go
@@ -37,13 +37,13 @@ func tubeMap(p, n vector.Vector3D) (res vector.Vector3D) {
 }
 
 func sphereMap(p, n vector.Vector3D) (res vector.Vector3D) {
-	d := p[vector.X] * p[vector.X] + p[vector.Y] * p[vector.Y] + p[vector.Z] * p[vector.Z]
+	d := p[vector.X]*p[vector.X] + p[vector.Y]*p[vector.Y] + p[vector.Z]*p[vector.Z]
 	if d > 0 {
 		res[vector.Z] = math.Sqrt(d)
 		if p[vector.X] != 0 && p[vector.Y] != 0 {
 			res[vector.X] = -math.Atan2(p[vector.X], p[vector.Y]) / math.Pi
 		}
-		res[vector.Y] = 1.0 - 2.0 * math.Acos(p[vector.Z] / res[vector.Z]) / math.Pi
+		res[vector.Y] = 1.0 - 2.0*math.Acos(p[vector.Z]/res[vector.Z])/math.Pi
 	}
 	return
 }
