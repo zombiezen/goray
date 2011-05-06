aa77261c55de3a990382163054ed6fc5fe27a669
Ross Light <ross@zombiezen.com>
2011-05-05 19:29:42 -0700

    Fix errors in fileloader package

--- src/goray/std/textures/image/fileloader/fileloader.go
+++ src/goray/std/textures/image/fileloader/fileloader.go
@@ -11,59 +11,55 @@ package fileloader
 import (
 	"os"
 
-	"image"
+	goimage "image"
 	_ "image/jpeg"
 	_ "image/png"
 
 	slashpath "path"
 	"path/filepath"
+
+	"goray/core/render"
+	"goray/std/textures/image"
 )
+
+func openImage(fspath string) (img *render.Image, err os.Error) {
+	f, err := os.Open(fspath)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	i, _, err := goimage.Decode(f)
+	if err != nil {
+		return
+	}
+	return render.NewGoImage(i), nil
+}
 
 // New creates an image loader that is rooted at a given directory.
 // Users of the loader will not directly be able to access anything outside the directory, but symlinks inside the directory will be followed.
 func New(base string) image.Loader {
-	return LoaderFunc(func(name string) (img *render.Image, err os.Error) {
+	return image.LoaderFunc(func(name string) (img *render.Image, err os.Error) {
 		if name == "" {
 			return nil, os.NewError("name must not be empty")
 		}
 		name = slashpath.Clean("/" + name)
-		p := slashpath.Join(base, filepath.FromSlash(name))
-		f, err := os.Open(p)
-		if err != nil {
-			return
-		}
-		defer f.Close()
-		i, _, err := image.Decode(f)
-		if err != nil {
-			return
-		}
-		return render.NewGoImage(i), nil
+		return openImage(slashpath.Join(base, filepath.FromSlash(name)))
 	})
 }
 
 // NewFull creates an image loader that defaults to the given directory.
 // Users of the loader can access anything in local storage.
 func NewFull(base string) image.Loader {
-	return LoaderFunc(func(name string) {
+	return image.LoaderFunc(func(name string) (img *render.Image, err os.Error) {
 		if name == "" {
 			return nil, os.NewError("name must not be empty")
 		}
-
 		var p string
 		if name[0] == '/' {
 			p = filepath.FromSlash(name)
 		} else {
-			p := slashpath.Join(base, filepath.FromSlash(name))
+			p = slashpath.Join(base, filepath.FromSlash(name))
 		}
-		f, err := os.Open(p)
-		if err != nil {
-			return
-		}
-		defer f.Close()
-		i, _, err := image.Decode(f)
-		if err != nil {
-			return
-		}
-		return render.NewGoImage(i), nil
+		return openImage(p)
 	})
 }
